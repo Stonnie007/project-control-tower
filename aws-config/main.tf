1a1796acd7ce8bbf69e925bee936240fc936e47d
Stonnie007 <emmanuelobiorah007@gmail.com>
2024-11-13 23:16:50 +0000

    project automation

---
+++ aws-config/main.tf
@@ -0,0 +1,133 @@
+##AWS Configuration rule
+resource "aws_config_config_rule" "r" {
+  name = "example"
+
+  source {
+    owner             = "AWS"
+    source_identifier = "S3_BUCKET_VERSIONING_ENABLED"
+  }
+
+  depends_on = [aws_config_configuration_recorder.foo]
+}
+
+resource "aws_config_configuration_recorder" "foo" {
+  name     = "example"
+  role_arn = aws_iam_role.r.arn
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "r" {
+  name               = "my-awsconfig-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "p" {
+  statement {
+    effect    = "Allow"
+    actions   = ["config:Put*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "p" {
+  name   = "my-awsconfig-policy"
+  role   = aws_iam_role.r.id
+  policy = data.aws_iam_policy_document.p.json
+}
+
+##AWS Configuration recorder
+resource "aws_config_configuration_recorder" "foo" {
+  name     = "example"
+  role_arn = aws_iam_role.r.arn
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "r" {
+  name               = "awsconfig-example"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+##AWS configuration recorder status
+resource "aws_config_configuration_recorder_status" "foo" {
+  name       = aws_config_configuration_recorder.foo.name
+  is_enabled = true
+  depends_on = [aws_config_delivery_channel.foo]
+}
+
+resource "aws_iam_role_policy_attachment" "a" {
+  role       = aws_iam_role.r.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWS_ConfigRole"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "awsconfig-example"
+}
+
+resource "aws_config_delivery_channel" "foo" {
+  name           = "example"
+  s3_bucket_name = aws_s3_bucket.b.bucket
+}
+
+resource "aws_config_configuration_recorder" "foo" {
+  name     = "example"
+  role_arn = aws_iam_role.r.arn
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["config.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "r" {
+  name               = "example-awsconfig"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "p" {
+  statement {
+    effect  = "Allow"
+    actions = ["s3:*"]
+    resources = [
+      aws_s3_bucket.b.arn,
+      "${aws_s3_bucket.b.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "p" {
+  name   = "awsconfig-example"
+  role   = aws_iam_role.r.id
+  policy = data.aws_iam_policy_document.p.json
+}
